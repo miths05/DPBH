10c48519ad169e81ec8934502b38f39b0f5c6ece
miths05 <119745912+miths05@users.noreply.github.com>
2024-02-01 01:00:00 +0530

    Delete extension directory

--- extension/background.js
+++
@@ -1,9 +0,0 @@
-// Listen for clicks on the extension icon
-chrome.action.onClicked.addListener((tab) => {
-    // Execute content script when the extension icon is clicked
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ['contentScript.js']
-    });
-  });
-  
